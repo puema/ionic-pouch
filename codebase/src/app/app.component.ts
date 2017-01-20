d926743e69ef1dbc996fc5f94e27a9551837c09a
Marcel <marcelpuetz@web.de>
2017-01-20 08:53:11 +0100

    clean repo

--- codebase/src/app/app.component.ts
+++ codebase/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
-
-import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
 
 
